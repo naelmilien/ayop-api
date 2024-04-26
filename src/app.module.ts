052c7d9ee5557b16e8535076eb96776942587920
Nate Milien <nathanael.milien@thoughtworks.com>
2024-04-25 23:58:09 -0400

    Format
    Format files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+import { PersonModule } from './person/person.module'
 
 @Module({
-  imports: [],
+  imports: [PersonModule],
   controllers: [AppController],
   providers: [AppService],
 })
