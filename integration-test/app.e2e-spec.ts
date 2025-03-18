943b967d22845eb6236263f0b7167dc337403edb
Nate Milien <nathanael.milien@thoughtworks.com>
2025-03-17 23:01:44 -0400

    Start basic database setup

--- integration-test/app.e2e-spec.ts
+++ integration-test/app.e2e-spec.ts
@@ -2,10 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let dataSource: DataSource;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,6 +16,19 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    dataSource = app.get(DataSource);
+  });
+
+  afterAll(async () => {
+    // TODO: Remove conditionals once we start building the database for real
+    if (app) {
+      await app.close();
+    }
+
+    if (dataSource && dataSource.isInitialized) {
+      await dataSource.destroy();
+    }
   });
 
   it('/ (GET)', () => {
