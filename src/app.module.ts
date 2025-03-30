7fd12b9a48ea308beffd5927dedf090e57b9cc9a
naelmilien <nmilien09@gmail.com>
2025-03-30 19:17:11 -0400

    Update database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Person } from './persons/person.entity';
 import { PersonsModule } from './persons/persons.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PersonsModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'root',
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.PORT ? parseInt(process.env.PORT) : 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
       database: 'ayop_test',
       entities: [Person],
       synchronize: true,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(`🚀 App listening on port ${process.env.PORT ?? 3000}`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
