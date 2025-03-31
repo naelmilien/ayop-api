ba55653eac0369d213043609e0472bd298eb4ae7
naelmilien <nmilien09@gmail.com>
2025-03-31 01:56:23 -0400

    Update database config variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,11 @@ import { PersonsModule } from './persons/persons.module';
     PersonsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.PGHOST,
-      port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
-      username: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      database: process.env.PGDATABASE,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Person],
       synchronize: process.env.NODE_ENV === 'test',
       ssl:
